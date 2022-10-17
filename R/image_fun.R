d6cabcdfcb86b57a2b628d2d0494b324a471cc8f
paul-goodall <paul.thomas.goodall@gmail.com>
2022-10-17 20:55:37 +1100

    adding the image functions

--- R/image_fun.R
+++ R/image_fun.R
@@ -1,7 +1,42 @@
 
+#' Returns a matrix of zeroes
+#'
+#' This function simply returns a matrix of zeroes with specified dimensions.
+#'
+#' @param x A matrix OR the x-dimensions of a matrix to be created.
+#' @param y (optional) the y-dimensions of a matrix to be created.
+#' @return A matrix of same dims as x or x*y
+#' @export
+zeroes <- function(x, y=1){
+  if(class(x)[1] == "matrix"){
+    y <- dim(x)[2]
+    x <- dim(x)[1]
+  }
+  my_m <- matrix(rep(0:(x-1),y), nrow=x)*0
+  return (my_m)
+}
+
+#' Returns a matrix of ones
+#'
+#' This function simply returns a matrix of ones with specified dimensions.
+#'
+#' @param x A matrix OR the x-dimensions of a matrix to be created.
+#' @param y (optional) the y-dimensions of a matrix to be created.
+#' @return A matrix of same dims as x or x*y
+#' @export
+ones <- function(x, y=1){
+  if(class(x)[1] == "matrix"){
+    y <- dim(x)[2]
+    x <- dim(x)[1]
+  }
+  my_m <- matrix(rep(0:(x-1),y), nrow=x)*0 + 1
+  return (my_m)
+}
+
+
 #' Returns the xval coordinate of a matrix
 #'
-#' This function simply returns a number correspondong to the x-index of a matrix, or creates a matrix with specified dimensions.
+#' This function simply returns a number corresponding to the x-index of a matrix, or creates a matrix with specified dimensions.
 #'
 #' @param x A matrix OR the x-dimensions of a matrix to be created.
 #' @param y (optional) the y-dimensions of a matrix to be created.
@@ -56,6 +91,22 @@ rvals <- function(x, y=1, xoffset=0, yoffset=0){
   return (my_r)
 }
 
+#' Returns the vvals of a matrix
+#'
+#' This function simply returns the element number of the matrix with specified dimensions (the pixel number for images)
+#'
+#' @param x A matrix OR the x-dimensions of a matrix to be created.
+#' @param y (optional) the y-dimensions of a matrix to be created.
+#' @return A matrix of same dims as x or x*y
+#' @export
+vvals <- function(x, y=1, xoffset=0, yoffset=0){
+  if(class(x)[1] == "matrix"){
+    y <- dim(x)[2]
+    x <- dim(x)[1]
+  }
+  my_x <- matrix(0:(x*y-1), nrow=x)
+  return (my_x)
+}
 
 
 
--- go_all.R
+++ go_all.R
@@ -4,14 +4,14 @@ library(devtools)
 
 devtools::document(setwd("/Volumes/Abyss/Dropbox/my_DataScience/R_PackageDev/funkyR"))
 
-test()
+#test()
 
 document()
 # use_readme_rmd() # only need to do this once
 devtools::build_readme() # builds README.md from README.Rmd
 document()
 
-check()
+#check()
 install()
 
 #usethis::use_github_actions()
